2e9ffb6e5c8b1284ce4e96b9c8e6498b0e56f41e
Borja Herrera <borja.a.herrera@gmail.com>
2025-03-06 14:07:06 +0100

    Renombrados archivos para resolver conflictos de mayúsculas y minúsculas

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,4 +1,4 @@
-import './Main.css';
+import './main.css';
 
 export const Main = () => {
   const main = document.createElement('main');
--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
-import { Main } from './components/main/main';
 import { header } from './components/header/header';
 import { Home } from './pages/Home/Home';
 import { popstate } from './utils/listeners/popState';
 import './style.css';
+import { Main } from './components/main/main';
 
 header();
 Main();
