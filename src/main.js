6d01c40645330f4c2dd93af8374914ea6c8498a1
Borja Herrera <borja.a.herrera@gmail.com>
2025-03-06 14:13:19 +0100

    Corregida la ruta de importación de Main.js

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import { header } from './components/header/header';
 import { Home } from './pages/Home/Home';
 import { popstate } from './utils/listeners/popState';
 import './style.css';
-import { Main } from './components/main/main';
+import { Main } from './components/Main/main';
 
 header();
 Main();
